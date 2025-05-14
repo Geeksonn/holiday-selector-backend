59d73e0390ed2a18b2f4c304af43b2cf9807ea7e
Olivier Sputael <sputael.o@gmail.com>
2025-05-14 21:15:29 +0200

    fix build error

--- app/api/holidays/route.ts
+++ app/api/holidays/route.ts
@@ -1,5 +1,6 @@
 import { getAllHolidays } from '@/app/lib/holidays';
 
 export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
     return Response.json(await getAllHolidays());
 }

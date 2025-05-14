55ad93099b31afdfe71ccfa4ff013fceca5a9edf
Olivier Sputael <sputael.o@gmail.com>
2025-05-14 21:18:15 +0200

    fix build error 2

--- app/api/holidays/route.ts
+++ app/api/holidays/route.ts
@@ -2,5 +2,6 @@ import { getAllHolidays } from '@/app/lib/holidays';
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
+    console.log('searchParams', searchParams);
     return Response.json(await getAllHolidays());
 }

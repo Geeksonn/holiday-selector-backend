d31616a93abdd6bed576632ac19ae09c27a0b183
Olivier Sputael <sputael.o@gmail.com>
2025-07-06 15:29:29 +0200

    Added confirmed email page

---
+++ app/email-confirmed/page.tsx
@@ -0,0 +1,8 @@
+export default function EmailConfirmedScreen() {
+    return (
+        <div className='p-4'>
+            <h1 className='text-3xl font-bold text-center'>Email Confirmed</h1>
+            <p className='text-center mt-4'>Thank you for confirming your email address!</p>
+        </div>
+    );
+}
